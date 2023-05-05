297deebe11f42aca8b7e308064e406eacb2745bc
Santiago Massa <sjm_95@hotmail.com>
2023-05-05 16:59:29 +0200

    1st push

---
+++ models/Movie.models.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+    
+})
+
+const Movies = mongoose.model("Movies", movieSchema)
+
